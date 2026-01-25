bc68960c64947d6d95bfa11ccd2631978e715f8d
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2026-01-25 20:04:44 +0530

    Updated code with comments

--- Graphs/Clone Graph.java
+++ Graphs/Clone Graph.java
@@ -1,48 +1,55 @@
 // Problem Link : https://leetcode.com/problems/clone-graph/description/
 
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
+
+    // HashMap to store mapping from original node -> cloned node
+    // This helps to:
+    // 1. Avoid cloning the same node multiple times
+    // 2. Handle cycles in the graph
     HashMap<Node, Node> cloneMap = new HashMap<>();
 
     public Node cloneGraph(Node node) {
-        if(node == null) return null;
-        
+
+        // Base case: if input graph is empty
+        if (node == null) return null;
+
+        // Create clone of the starting node
         Node clonedNode = new Node(node.val);
+
+        // Store the mapping: original node -> cloned node
         cloneMap.put(node, clonedNode);
 
+        // Perform DFS to clone the entire graph
         dfs(node, clonedNode);
+
+        // Return the cloned starting node
         return clonedNode;
     }
+
+    // DFS function to traverse original graph and build cloned graph
     void dfs(Node node, Node clonedNode) {
 
-        for(Node neighbor : node.neighbors) {
-            if(!cloneMap.containsKey(neighbor)) {
+        // Traverse all neighbors of the current original node
+        for (Node neighbor : node.neighbors) {
+
+            // If this neighbor is not yet cloned
+            if (!cloneMap.containsKey(neighbor)) {
+
+                // Create a clone of the neighbor node
                 Node clonedNeighbor = new Node(neighbor.val);
+
+                // Store mapping in HashMap
                 cloneMap.put(neighbor, clonedNeighbor);
+
+                // Add cloned neighbor to the current cloned node's neighbor list
                 clonedNode.neighbors.add(clonedNeighbor);
 
+                // Recursively clone the neighbor's neighbors
                 dfs(neighbor, clonedNeighbor);
-            } 
+            }
             else {
+                // If neighbor is already cloned (cycle or previously visited),
+                // directly add the already created cloned node
                 clonedNode.neighbors.add(cloneMap.get(neighbor));
             }
         }
